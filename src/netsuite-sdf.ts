de0216d394fe272a2ea6fd5743afed509e333bed
Mark Keaton <mark.keaton@nwyc.com>
2018-04-13 23:37:43 -0500

    Update dependencies. Remove unused imports.

--- src/netsuite-sdf.ts
+++ src/netsuite-sdf.ts
@@ -7,13 +7,9 @@ import { ChildProcess } from 'child_process';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/concatMap';
-import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/scan';
-import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/toPromise';
 
 import { spawn } from 'spawn-rx';
