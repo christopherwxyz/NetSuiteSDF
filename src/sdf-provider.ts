293a8e45f5b7ce95805df7cb0552b16f3fbc6273
Mark Keaton <mark.keaton@nwyc.com>
2018-04-21 17:40:37 -0500

    Dynamically pull SDF Object in Tree Provider.

--- src/sdf-provider.ts
+++ src/sdf-provider.ts
@@ -4,10 +4,10 @@ import * as path from 'path';
 
 import * as _ from 'lodash';
 import { NetSuiteSDF } from './netsuite-sdf';
-import { CustomObjects } from './custom-object';
+import { CustomObjects, CustomObject } from './custom-object';
+import { CLICommand } from './cli-command';
 
 type FilePath = string;
-type CustomObject = string;
 
 export class SDFProvider implements vscode.TreeDataProvider<SDFFile> {
 
@@ -24,15 +24,12 @@ export class SDFProvider implements vscode.TreeDataProvider<SDFFile> {
     return element;
   }
 
-  async getChildren(element?: SDFFile | SDFFolder) {
+  async getChildren(element?: SDFFile | SDFFolder | SDFObjectFolder) {
     if (element && element.label === 'SuiteScripts') {
       await this.getSuiteScripts(<SDFFolder>element);
       return (<SDFFolder>element).getChildren();
-    } else if (element && element.label === 'Objects') {
-      await this.getObjects(<SDFFolder>element);
-      return (<SDFFolder>element).getChildren();
     } else if (element) {
-      return (<SDFFolder>element).getChildren();
+      return (<SDFFolder | SDFObjectFolder>element).getChildren();
     } else {
       return [
         this.getObjectFolders(),
@@ -80,10 +77,13 @@ export class SDFProvider implements vscode.TreeDataProvider<SDFFile> {
     const objects = new SDFFolder('Objects');
     const objectsMap = _.reduce(CustomObjects, (acc, customObject) => {
       const folderName = customObject._destination[0];
-      const child = folderName in acc ? acc[folderName] : new SDFFolder(folderName);
+      let child: SDFFolder | SDFObjectFolder;
       if (customObject._destination.length > 1) {
+        child = folderName in acc ? acc[folderName] : new SDFFolder(folderName);
         const grandchild = customObject._destination[1];
-        child.children.push(new SDFFolder(grandchild));
+        (<SDFFolder>child).children.push(new SDFObjectFolder(this.sdf, grandchild, customObject));
+      } else {
+        child = new SDFObjectFolder(this.sdf, folderName, customObject);
       }
       acc[folderName] = child;
       return acc;
@@ -92,48 +92,12 @@ export class SDFProvider implements vscode.TreeDataProvider<SDFFile> {
     return objects;
   }
 
-  private async getObjects(suiteScriptsFolder: SDFFolder) {
-    this.sdf.doAddProjectParameter = false;
-    this.sdf.doReturnData = true;
-
-    const collectedData = await this.sdf.listFiles();
-    if (collectedData) {
-      const reducedData = _(collectedData)
-        .filter(path => path)
-        .map(path => path.split('/').slice(2))
-        .reduce((acc, pathParts, index) => {
-          const path = collectedData[index];
-          const fileName = pathParts[pathParts.length - 1];
-
-          if (pathParts.length === 1) {
-            acc.children.push(new SDFFile(fileName, path));
-          } else {
-            const dirParts = pathParts.slice(0, -1);
-
-            let dirObj = acc;
-            while (!_.isEmpty(dirParts)) {
-              let dir = dirParts.shift();
-              if (!_.has(dirObj, dir)) {
-                dirObj[dir] = new SDFFolder(dir);
-              }
-              dirObj = dirObj[dir];
-            }
-
-            dirObj['children'].push(new SDFFile(fileName, path));
-          }
-          return acc;
-        }, suiteScriptsFolder);
-      console.log(reducedData);
-    }
-  }
-
 }
 
 export class SDFFile extends vscode.TreeItem {
 
   constructor(
     public readonly label: string,
-    // public readonly command?: vscode.Command
     private path: string,
   ) {
     super(label, vscode.TreeItemCollapsibleState.None);
@@ -149,11 +113,10 @@ export class SDFFile extends vscode.TreeItem {
 
 export class SDFFolder extends vscode.TreeItem {
 
-  children: Array<SDFFolder | SDFFile> = [];
+  children: Array<SDFFolder | SDFFile | SDFObjectFolder> = [];
 
   constructor(
     public label: string,
-    // public readonly command?: vscode.Command
   ) {
     super(label, vscode.TreeItemCollapsibleState.Collapsed);
   }
@@ -172,4 +135,32 @@ export class SDFFolder extends vscode.TreeItem {
 
   contextValue = 'sdf-folder';
 
+}
+
+export class SDFObjectFolder extends vscode.TreeItem {
+
+  constructor(
+    public sdf: NetSuiteSDF,
+    public label: string,
+    public object: CustomObject,
+  ) {
+    super(label, vscode.TreeItemCollapsibleState.Collapsed);
+  }
+
+  get tooltip(): string {
+    return `${this.label}`
+  }
+
+  async getChildren() {
+    this.sdf.doAddProjectParameter = false;
+    this.sdf.doReturnData = true;
+
+    await this.sdf.getConfig();
+    if (this.sdf.sdfConfig && this.sdf.password) {
+      const files = await this.sdf.runCommand(CLICommand.ListObjects, `-type ${this.object.type}`);
+      return _.map(files, (file: string) => new SDFFile(file, this.object.destination));
+    }
+  }
+
+  contextValue = 'sdf-object-folder';
 }
